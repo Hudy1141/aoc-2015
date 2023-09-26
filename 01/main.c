f417fabddcfbac4c8d270534bcc78cf045a322df
Hudy <hudy1141@gmail.com>
2023-09-26 22:18:35 +0200

    Minor code changes for day 1

--- 01/main.c
+++ 01/main.c
@@ -9,32 +9,37 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    int direction;
-    int floor = 0;
-    int counter = 0;
-    int position = 0;
-
     FILE *f = fopen(argv[1], "r");
     if (f == NULL) {
         perror("Error");
         return -1;
     }
+
+    int direction;
+    int floor = 0;
+    int counter = 0;
+    int position = 0;
+
     while (1) {
         direction = fgetc(f);
+
         if (feof(f)) {
             break;
         }
+
         if (direction == '(') {
             floor++;
         }
         else {
             floor--;
         }
+
         counter++;
         if (floor == -1 && position == 0) {
             position = counter;
         }
     }
+
     fclose(f);
 
     // To what floor do the instructions take Santa?
